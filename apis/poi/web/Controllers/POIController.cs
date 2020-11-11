a2ecc429a288e4e00dff30feb77acc4ed7d0ffd3
divzi-p <38757978+divzi-p@users.noreply.github.com>
2020-11-11 19:45:51 +0530

    Update POIController.cs

--- apis/poi/web/Controllers/POIController.cs
+++ apis/poi/web/Controllers/POIController.cs
@@ -11,7 +11,7 @@ namespace poi.Controllers
     [Route("api/poi")]
     public class POIController : ControllerBase
     {
-        //test modification 6
+        //test modification 7
         private readonly POIContext _context;
 
         public POIController(POIContext context)
