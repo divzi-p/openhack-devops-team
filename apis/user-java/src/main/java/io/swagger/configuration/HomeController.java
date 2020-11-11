7fd9d3e8013c0b70723997122ff7ead4e4d77564
divzi-p <38757978+divzi-p@users.noreply.github.com>
2020-11-11 19:37:48 +0530

    Update HomeController.java

--- apis/user-java/src/main/java/io/swagger/configuration/HomeController.java
+++ apis/user-java/src/main/java/io/swagger/configuration/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class HomeController {
-    //controller comment 1
+    //controller comment 2
     @RequestMapping(value = "/")
     public String index() {
         System.out.println("swagger-ui.html");
