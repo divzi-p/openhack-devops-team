17bf0785bbe60da460e3f5cd85591059ef782a69
divzi-p <38757978+divzi-p@users.noreply.github.com>
2020-11-12 17:48:10 +0530

    Update server.js

--- apis/userprofile/server.js
+++ apis/userprofile/server.js
@@ -10,7 +10,7 @@ var sqlConfig = require('./config/sqlConfig');
 var morgan = require('morgan');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./config/swagger.json');
-
+//test comment 1
 var App = Express();
 
 var Server = Http.createServer(App);
